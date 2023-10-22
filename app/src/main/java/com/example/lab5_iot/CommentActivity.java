060aa782aac2cef3acdba0214f68aaf584c8af9c
DiegoLG15 <a20200839@pucp.edu.pe>
2023-10-22 18:05:51 -0500

    vistas principales listas

---
+++ app/src/main/java/com/example/lab5_iot/CommentActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab5_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab5_iot.databinding.ActivityCommentBinding;
+import com.example.lab5_iot.databinding.ActivityMainBinding;
+
+public class CommentActivity extends AppCompatActivity {
+
+    private ActivityCommentBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCommentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
--- app/src/main/java/com/example/lab5_iot/MainActivity.java
+++ app/src/main/java/com/example/lab5_iot/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.lab5_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lab5_iot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private Button iniciarBtnTutor;
+    private Button iniciarBtnTrabajador;
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        iniciarBtnTutor=findViewById(R.id.btnTutor);
+        iniciarBtnTrabajador=findViewById(R.id.btnTrabajador);
+        iniciarBtnTutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TutorActivity.class);
+                startActivity(intent);
+            }
+        });
+        iniciarBtnTrabajador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Iniciar la actividad de Trabajador aquí
+                Intent intent = new Intent(MainActivity.this, TrabajadorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
