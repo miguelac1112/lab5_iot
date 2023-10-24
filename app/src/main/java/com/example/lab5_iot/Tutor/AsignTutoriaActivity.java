c7bf158b0972767118989c9f16302e7200a277dd
Miguel <a20190050@pucp.edu.pe>
2023-10-24 03:08:49 -0500

    avance asignar tutoria

--- app/src/main/java/com/example/lab5_iot/Tutor/AsignTutoriaActivity.java
+++ app/src/main/java/com/example/lab5_iot/Tutor/AsignTutoriaActivity.java
@@ -3,17 +3,25 @@ package com.example.lab5_iot.Tutor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.lab5_iot.R;
 import com.example.lab5_iot.databinding.ActivityAsignTutoriaBinding;
 
 public class AsignTutoriaActivity extends AppCompatActivity {
 
     private ActivityAsignTutoriaBinding binding;
+    private Button asignarTutoriaButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAsignTutoriaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        asignarTutoriaButton=findViewById(R.id.asignarTutoriaButton);
+        asignarTutoriaButton.setOnClickListener(view ->{
+
+        });
 
     }
 }
